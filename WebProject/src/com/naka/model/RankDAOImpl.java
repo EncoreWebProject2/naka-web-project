a2e3ecc251f68ff7b20de5e929a061ffb72962c5
flo2seul <seulaayoon@gmail.com>
2021-05-04 14:28:14 +0900

    ranking.jsp 시작

--- WebProject/src/com/naka/model/RankDAOImpl.java
+++ WebProject/src/com/naka/model/RankDAOImpl.java
@@ -11,15 +11,13 @@ import java.util.Map;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
-
 import com.naka.vo.RankVO;
 import com.naka.vo.RecruitVO;
 
-
-public class RankDAOImpl implements RankDAO{
+public class RankDAOImpl implements RankDAO {
 	private static RankDAOImpl instance = new RankDAOImpl();
 	private DataSource ds;
-	
+
 	public RankDAOImpl() {
 //		try {
 //			Class.forName(ServerInfo.DRIVER_NAME);
@@ -30,22 +28,21 @@ public class RankDAOImpl implements RankDAO{
 //		}
 		try {
 			InitialContext ic = new InitialContext();
-			ds = (DataSource)ic.lookup("java:comp/env/jdbc/mysql");
-		}catch(Exception e) {
+			ds = (DataSource) ic.lookup("java:comp/env/jdbc/mysql");
+		} catch (Exception e) {
 			System.err.println("Datasource create Error");
 		}
 	}
-	
+
 	public static RankDAO getInstance() {
 		return instance;
 	}
-	
-	
-	  @Override 
-	  public Connection getConnection() throws SQLException { 
-		  return ds.getConnection();
-	  }
-	 
+
+	@Override
+	public Connection getConnection() throws SQLException {
+		return ds.getConnection();
+	}
+
 //	public Connection getConnection() throws SQLException {		
 //		System.out.println("디비연결 성공....");
 //		return DriverManager.getConnection(ServerInfo.URL,ServerInfo.USER, ServerInfo.PASS);
@@ -53,9 +50,9 @@ public class RankDAOImpl implements RankDAO{
 	@Override
 	public void closeAll(PreparedStatement ps, Connection con) {
 		try {
-			if(ps!=null)
+			if (ps != null)
 				ps.close();
-			if(con!=null)
+			if (con != null)
 				con.close();
 		} catch (SQLException e) {
 		}
@@ -63,11 +60,11 @@ public class RankDAOImpl implements RankDAO{
 
 	@Override
 	public void closeAll(ResultSet rs, PreparedStatement ps, Connection con) {
-		closeAll(ps,con);
+		closeAll(ps, con);
 		try {
-			if(rs!=null)
+			if (rs != null)
 				rs.close();
-		}catch (Exception e) {
+		} catch (Exception e) {
 		}
 	}
 
@@ -76,30 +73,27 @@ public class RankDAOImpl implements RankDAO{
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-		
+
 		ArrayList<RankVO> list = new ArrayList<RankVO>();
 		try {
 			con = getConnection();
 			String query = "SELECT name,value FROM ranking WHERE id LIKE 't%' ORDER BY value DESC LIMIT 20;";
 			ps = con.prepareStatement(query);
-			
+
 			rs = ps.executeQuery();
-			while(rs.next()) {
-				if (rs.getString("name")!="") {
-					list.add(new RankVO(rs.getString("name"),rs.getInt("value")));
-					
-				}
-				else {
-					
+			while (rs.next()) {
+				if (rs.getString("name") != "") {
+					list.add(new RankVO(rs.getString("name"), rs.getInt("value")));
+
+				} else {
+
 					continue;
-					}
 				}
-				
-			
-			
-		}catch(Exception e) {
-			
-		}finally {
+			}
+
+		} catch (Exception e) {
+
+		} finally {
 			closeAll(rs, ps, con);
 		}
 		return list;
@@ -110,21 +104,21 @@ public class RankDAOImpl implements RankDAO{
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-		
+
 		ArrayList<RankVO> list = new ArrayList<RankVO>();
 		try {
 			con = getConnection();
 			String query = "SELECT name,value FROM ranking WHERE id LIKE 'p%' ORDER BY value DESC LIMIT 10;";
 			ps = con.prepareStatement(query);
 			rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				list.add(new RankVO(rs.getString("name"),rs.getInt("value")));
+
+			while (rs.next()) {
+				list.add(new RankVO(rs.getString("name"), rs.getInt("value")));
 			}
-			
-		}catch(Exception e) {
-			
-		}finally {
+
+		} catch (Exception e) {
+
+		} finally {
 			closeAll(rs, ps, con);
 		}
 		return list;
@@ -135,46 +129,45 @@ public class RankDAOImpl implements RankDAO{
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-		
+
 		ArrayList<RankVO> list = new ArrayList<RankVO>();
 		try {
 			con = getConnection();
 			String query = "SELECT name,value FROM ranking WHERE id LIKE 'j%' ORDER BY value;";
 			ps = con.prepareStatement(query);
 			rs = ps.executeQuery();
-			while(rs.next()) {
-				list.add(new RankVO(rs.getString("name"),rs.getInt("value")));
+			while (rs.next()) {
+				list.add(new RankVO(rs.getString("name"), rs.getInt("value")));
 			}
-			
-		}catch(Exception e) {
-			
-		}finally {
+
+		} catch (Exception e) {
+
+		} finally {
 			closeAll(rs, ps, con);
 		}
 		return list;
 	}
-	
 
 	@Override
 	public void refreshRank() {
 		ArrayList<RecruitVO> list = new ArrayList<RecruitVO>();
 		list.add(null);
-		
-		HashMap<String,Integer> tech_map = new HashMap<String,Integer>();
-		HashMap<String,Integer> position_map = new HashMap<String,Integer>();
-		HashMap<String,Integer> job_type_map = new HashMap<String,Integer>();
-		
-		for(int i=1;list.size()!=0;i++) {
-			list = RecruitDAOImpl.getInstance().getRecruits(i);			
-			for(RecruitVO vo : list) {
-				for(String s : vo.getTech().split(", ")) {
-					tech_map.put(s, tech_map.getOrDefault(s, 0)+1);	
+
+		HashMap<String, Integer> tech_map = new HashMap<String, Integer>();
+		HashMap<String, Integer> position_map = new HashMap<String, Integer>();
+		HashMap<String, Integer> job_type_map = new HashMap<String, Integer>();
+
+		for (int i = 1; list.size() != 0; i++) {
+			list = RecruitDAOImpl.getInstance().getRecruits(i);
+			for (RecruitVO vo : list) {
+				for (String s : vo.getTech().split(", ")) {
+					tech_map.put(s, tech_map.getOrDefault(s, 0) + 1);
 				}
-				for(String s : vo.getPosition().split(", ")) {
-					position_map.put(s, position_map.getOrDefault(s, 0)+1);
+				for (String s : vo.getPosition().split(", ")) {
+					position_map.put(s, position_map.getOrDefault(s, 0) + 1);
 				}
-				for(String s : vo.getJob_type().split("/")) {
-					job_type_map.put(s, job_type_map.getOrDefault(s, 0)+1);
+				for (String s : vo.getJob_type().split("/")) {
+					job_type_map.put(s, job_type_map.getOrDefault(s, 0) + 1);
 				}
 			}
 		}
@@ -182,41 +175,37 @@ public class RankDAOImpl implements RankDAO{
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		int i = 1;
-		
+
 		try {
 			con = getConnection();
 			String query = "INSERT INTO ranking (id, name, value) VALUES(?,?,?)";
 			ps = con.prepareStatement(query);
-		
-			
+
 			for (Map.Entry<String, Integer> entry : tech_map.entrySet()) {
-			  ps.setString(1,"t_" + i++); 
-			  ps.setString(2, entry.getKey());
-			  ps.setInt(3,entry.getValue()); 
-			  ps.executeUpdate();
-            } 
+				ps.setString(1, "t_" + i++);
+				ps.setString(2, entry.getKey());
+				ps.setInt(3, entry.getValue());
+				ps.executeUpdate();
+			}
 			for (Map.Entry<String, Integer> entry : position_map.entrySet()) {
-			  ps.setString(1,"p_" + i++); 
-			  ps.setString(2, entry.getKey());
-			  ps.setInt(3,entry.getValue()); 
-			  ps.executeUpdate();
+				ps.setString(1, "p_" + i++);
+				ps.setString(2, entry.getKey());
+				ps.setInt(3, entry.getValue());
+				ps.executeUpdate();
 			}
 			for (Map.Entry<String, Integer> entry : job_type_map.entrySet()) {
-			  ps.setString(1,"j_" + i++); 
-			  ps.setString(2, entry.getKey());
-			  ps.setInt(3,entry.getValue()); 
-			  ps.executeUpdate();
+				ps.setString(1, "j_" + i++);
+				ps.setString(2, entry.getKey());
+				ps.setInt(3, entry.getValue());
+				ps.executeUpdate();
 			}
- 
-		    
-		   
-			
-		}catch (Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
-			closeAll(rs,ps,con);
+		} finally {
+			closeAll(rs, ps, con);
 		}
-		
+
 	}
 //	public static void main(String[] args) throws Exception {
 //		RankDAOImpl dao = new RankDAOImpl();
